c963a0debf42cf61daddc5552992dbac351cdba8
yassin mohamed <yassin_mohamed310@outlook.com>
2022-08-22 19:34:31 +0200

    add filter logic

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './filter.css'
 import { BsSearch } from 'react-icons/bs'
-const Filter = () => {
+const Filter = ({sort,size,handleFilterByOrder,handleFilterBySize}) => {
     return (
         <>
             <div className="search">
@@ -10,25 +10,25 @@ const Filter = () => {
             </div>
             <div className="productFilterTyps-section">
                 <h5 className="num-of-products">Number of Products : 12</h5>
-                    <div className="filter-by-size">
-                        <span>filter by size: </span>
-                        <select className="filter-select">
-                            <option value="all">All</option>
-                            <option value="x">X</option>
-                            <option value="m">M</option>
-                            <option value="l">L</option>
-                            <option value="xl">XL</option>
-                            <option value="xxl">XXL</option>
-                        </select>
-                    </div>
-                    <div className="filter-by-order">
-                        <span>filter by order: </span>
-                        <select className="filter-select">
-                            <option value="lastes">Lastes</option>
-                            <option value="lower">Lower</option>
-                            <option value="highest">Lighest</option>
-                        </select>
-                    </div>
+                <div className="filter-by-size">
+                    <span>filter by size: </span>
+                    <select value={size} className="filter-select" onChange={handleFilterBySize}>
+                        <option value="all">All</option>
+                        <option value="x">X</option>
+                        <option value="m">M</option>
+                        <option value="l">L</option>
+                        <option value="xl">XL</option>
+                        <option value="xxl">XXL</option>
+                    </select>
+                </div>
+                <div className="filter-by-order">
+                    <span>filter by order: </span>
+                    <select value={sort} className="filter-select" onChange={handleFilterByOrder}>
+                        <option value="lastes">Lastes</option>
+                        <option value="lowest">Lowest</option>
+                        <option value="highest">Highest</option>
+                    </select>
+                </div>
             </div>
         </>
     )
--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -7,10 +7,35 @@ import Filter from '../../components/Filter/Filter'
 
 const Shop = () => {
     const [products, setProducts] = useState(productData)
+    const [size, setSize] = useState('')
+    const [sort, setSort] = useState('')
+
+
+    const handleFilterBySize = (e) => {
+        setSize(e.target.value);
+        if(e.target.value === 'all'){
+            setProducts(productData)
+        }else{
+            let productsClone=[...products];
+            let newProducts=productsClone.filter(ele=>ele.size.indexOf(e.target.value) != -1)
+            console.log("newProducts",newProducts);
+            setProducts(newProducts)
+        }
+    }
+    const handleFilterByOrder = (e) => {
+        setSort(e.target.value);
+    }
+
+
     return (
         <div className="shop-wrapper">
             <div className="filter-section">
-                <Filter />
+                <Filter
+                size={size}
+                sort={sort}
+                handleFilterByOrder={handleFilterByOrder}
+                handleFilterBySize={handleFilterBySize}
+                products={products}/>
             </div>
             <div className="products-section">
                 <Products products={products}/>
